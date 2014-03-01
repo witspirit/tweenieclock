bb3f9808d35cc5c72d71df75ac38533d8ff20dc7
witspirit <witspirit@gmail.com>
2014-03-02 00:03:04 +0100

    Nice, have a relative scaling solution with accent areas. Should still add the missing areas.But we have a start.

--- javafx/src/main/java/be/witspirit/tweenieclock/TweenieClock.java
+++ javafx/src/main/java/be/witspirit/tweenieclock/TweenieClock.java
@@ -1,15 +1,21 @@
 package be.witspirit.tweenieclock;
 
+import javafx.animation.ScaleTransition;
 import javafx.application.Application;
+import javafx.beans.binding.DoubleBinding;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class TweenieClock extends Application {
 
@@ -22,27 +28,41 @@ public class TweenieClock extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Tweenie Clock");
 
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root, 300, 250);
+        AnchorPane root = new AnchorPane();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
 
         Image clock = new Image("TweenieClock.jpg");
         ImageView clockView = new ImageView(clock);
         clockView.setPreserveRatio(true);
-        clockView.fitWidthProperty().bind(scene.widthProperty());
-        clockView.fitHeightProperty().bind(scene.heightProperty());
+        AnchorPane.setTopAnchor(clockView, 0.0);
+        AnchorPane.setLeftAnchor(clockView, 0.0);
+        AnchorPane.setBottomAnchor(clockView, 0.0);
+        AnchorPane.setRightAnchor(clockView, 0.0);
         root.getChildren().add(clockView);
 
         Image center = new Image("detail_purple_center.png");
         ImageView centerHighlight = new ImageView(center);
-        StackPane.setAlignment(centerHighlight, Pos.TOP_LEFT);
+        AnchorPane.setLeftAnchor(centerHighlight, 1089.0);
+        AnchorPane.setTopAnchor(centerHighlight, 919.0);
 
+        ColorAdjust highlight = new ColorAdjust();
+        // highlight.setBrightness(-0.1);
+        highlight.setSaturation(0.2);
+        centerHighlight.setEffect(highlight);
 
 
         root.getChildren().add(centerHighlight);
 
+        DoubleBinding heightScale = scene.heightProperty().divide(clock.getHeight());
+        DoubleBinding widthScale = scene.widthProperty().divide(clock.getWidth());
+
+        Scale scale = new Scale();
+        scale.yProperty().bind(heightScale);
+        scale.xProperty().bind(widthScale);
+        root.getTransforms().add(scale);
 
 
-        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
